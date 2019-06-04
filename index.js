00e7e573ad16329eb68094e3641fe4fb50fdbb52
Shadowborn <james.starks023@gmail.com>
2019-06-04 15:44:43 -0600

    Forgot to commit all day.. All CRUD operations working with knex though!

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const server = express();
-
+// const server = express();
+const server = require('./api/server.js');
 server.use(express.json());
 server.use(helmet());
 
